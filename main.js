8e3cab777883bcc2620deed0810f50a4596781a2
atripodi <aldanat95@gmail.com>
2022-04-30 10:46:06 -0300

    JS Agrego función para mostrar Âproductos en carrito

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-// inventario
-
-const productos = [
-    {
-        id: item1,
-        nombre: "Taza serie 001",
-        tipo: "buzo",
-        desc: "Taza de cerámica realizada a mano en torno alfarero esmaltada jaspeada.",
-        precio: 500,
-        cantidad:1
-    },
-
-    {
-        id: item2,
-        nombre: "Jarra serie 001",
-        tipo: "remera",
-        desc: "Mini jarra de cerámica realizada a mano en torno alfarero esmaltada color blanco.",
-        precio: 500,
-        cantidad:1
-    }
-]
 // modal --> lo reemplazo por offcanvas
 
 // abrir carrito
@@ -40,21 +19,15 @@ const productos = [
 
 //agregar item al carrito
 
-const botonAgregarAlCarrito = document.getElementById("agregar1");
-//botonAgregarAlCarrito.onclick = () => { alert ("Agregaste este producto al carrito")};
+// const botonAgregarAlCarrito = document.getElementById("agregar1");
+// //botonAgregarAlCarrito.onclick = () => { alert ("Agregaste este producto al carrito")};
 
+// botonAgregarAlCarrito.addEventListener ('click',() => { 
+//     Swal.fire({
+//         text: 'Agregaste este producto al carrito!',
+//         icon: 'success',
+//         confirmButtonText: 'Cerrar',
+//     })
+// });
 
-botonAgregarAlCarrito.addEventListener ('click',() => { 
-    Swal.fire({
-        text: 'Agregaste este producto al carrito!',
-        icon: 'success',
-        confirmButtonText: 'Cerrar',
-    })
-});
-
-// detalle del carrito
-
-let carrito = []; 
-
-const detalleCarrito = document.querySelector('.modal-content'); // contenido del carrito
 
